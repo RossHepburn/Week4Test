a5ac553d03fdc3fb101a38fccc802da6a1efd92a
Ross Hepburn <hepburnjrl@gmail.com>
2014-03-16 23:51:00 +0000

    Can now total orders - refactored

--- lib/task2.rb
+++ lib/task2.rb
@@ -1,10 +1,14 @@
 class Takeaway
 
-	Dishes = {
-		:pizza => 10,
-		:curry => 15,
-		:steak => 20,
-		:octopus => 30
-	}
+attr_reader :dishes
 
+	def initialize
+ 	 @dishes = {'pizza' => 10, 'curry' => 15, 'steak' => 20, 'octopus' => 30}
+	end
+
+	def reciept(orders)
+		total = []
+		orders.each { |order| total << dishes[order] }
+		total.reduce { |sum,x| sum+x}
+	end
 end 
--- spec/task2_spec.rb
+++ spec/task2_spec.rb
@@ -1,13 +1,18 @@
 require 'task2'
  
 describe Takeaway do 
+
+	let(:takeaway) {Takeaway.new}
  
    it "should have list of prices for each dish" do
-     expect(Takeaway::Dishes[:pizza]).to eq(10)
-     expect(Takeaway::Dishes[:curry]).to eq(15)
-     expect(Takeaway::Dishes[:steak]).to eq(20)
-     expect(Takeaway::Dishes[:octopus]).to eq(30)
+     expect(takeaway.dishes['pizza']).to eq 10 
+     expect(takeaway.dishes['curry']).to eq 15 
+     expect(takeaway.dishes['steak']).to eq 20 
+     expect(takeaway.dishes['octopus']).to eq 30
    end
 
-   
+   it "should calculate the total sum of the order" do
+   	expect(takeaway.reciept(['pizza' , 'curry' , 'steak' , 'octopus'])).to eq 75
+   end
+
 end
