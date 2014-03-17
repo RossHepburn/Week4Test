1e24778d7ec479dcb7c91df1e03bfed8ce45bdcc
Ross Hepburn <hepburnjrl@gmail.com>
2014-03-17 00:08:03 +0000

    removed extra def

--- lib/task2.rb
+++ lib/task2.rb
@@ -6,12 +6,6 @@ attr_reader :dishes
  	 @dishes = {'pizza' => 10, 'curry' => 15, 'steak' => 20, 'octopus' => 30}
 	end
 
-	def reciept(orders)
-		total = []
-		orders.each { |order| total << dishes[order] }
-		total.reduce { |sum,x| sum+x}
-	end
-
 def reciept(orders, total) 
     sub_total = orders.map { |k,v| @dishes[k]*v }
     total == sub_total.reduce { |sum,x| sum+x }
